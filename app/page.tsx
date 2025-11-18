bf1b500ab0f88dd985c8a9a0a06cf0c7dd628177
eliashezron <eliashezron23@gmail.com>
2025-11-18 09:39:55 -0300

    footer removed

--- app/page.tsx
+++ app/page.tsx
@@ -15,9 +15,9 @@ export default function Home() {
       <MidSectionHero />
       <About />
       <Tokenized />
-      <div className="relative "> 
+      {/* <div className="relative "> 
       <Footer />
-      </div> 
+      </div>  */}
     </div>
   );
 }
