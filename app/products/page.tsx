84bfa6209f82466322ae4a3cca79afa3e60f7094
eliashezron <eliashezron23@gmail.com>
2025-11-18 08:58:37 -0300

    attempt 9

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,23 +1,23 @@
-import Footer from "../components/Footer";
-import ProductHero from "./ProductsHero";
-import ProductsSection from "./ProductsSection";
-export default function ProductsPage() {
-  return (
-    <div className="overflow-y-scroll h-screen" data-scroll-container="main">
-      {/* Hero */}
-      <div className="snap-start">
-      <ProductHero />
-      </div>
+// import Footer from "../components/Footer";
+// import ProductHero from "./ProductsHero";
+// import ProductsSection from "./ProductsSection";
+// export default function ProductsPage() {
+//   return (
+//     <div className="overflow-y-scroll h-screen" data-scroll-container="main">
+//       {/* Hero */}
+//       <div className="snap-start">
+//       <ProductHero />
+//       </div>
 
-      {/* Content */}
-       <div className="snap-start">
-      <ProductsSection />
-      </div>
+//       {/* Content */}
+//        <div className="snap-start">
+//       <ProductsSection />
+//       </div>
 
-      {/* Footer */}
-        <div className="snap-start">
-        <Footer />
-        </div>
-    </div>
-  );
-}
+//       {/* Footer */}
+//         <div className="snap-start">
+//         <Footer />
+//         </div>
+//     </div>
+//   );
+// }
